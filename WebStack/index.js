14fee43f8af3d2526abee35f87ce48424df83f3b
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-09-15 22:14:10 +0300

    dom Operations

--- WebStack/index.js
+++ WebStack/index.js
@@ -1,12 +1,20 @@
-function add(val1, val2) {
-    return val1 + val2;
-}
+// function add(val1, val2) {
+//     return val1 + val2;
+// }
 
-let ans = add(45, 45);
-console.log(ans)
+// let ans = add(45, 45);
+// console.log(ans)
 
-let square = function(number) {
-    return number * number;
-}
+// let square = function(number) {
+//     return number * number;
+// }
 
-console.log(square(9));
+// console.log(square(9));
+
+
+var firstItem = document.getElementById("img1"),
+card = document.getElementsByClassName("card");
+
+console.log(card);
+console.log(firstItem);
+
