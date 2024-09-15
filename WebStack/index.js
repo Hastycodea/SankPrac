786f34010863783effd37d8a0648c46dacd34e66
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-09-15 20:02:10 +0300

    WebFundamentals

---
+++ WebStack/index.js
@@ -0,0 +1,12 @@
+function add(val1, val2) {
+    return val1 + val2;
+}
+
+let ans = add(45, 45);
+console.log(ans)
+
+let square = function(number) {
+    return number * number;
+}
+
+console.log(square(9));
