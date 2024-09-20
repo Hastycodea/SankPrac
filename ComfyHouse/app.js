1c02ccc925aa994bdcc392d9a6267b88feeac096
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-09-20 12:26:19 +0300

    displayCartItems

--- ComfyHouse/app.js
+++ ComfyHouse/app.js
@@ -91,6 +91,8 @@ class UI {
                     this.setCartValues(cart);
 
                     // display cart items
+                    this.addCartItem(cartItem);
+
                     // show the cart
                 });
             
@@ -109,7 +111,28 @@ class UI {
         cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
         cartItems.innerText = itemsTotal;
 
-        console.log(cartTotal, cartItems);
+        // console.log(cartTotal, cartItems);
+    }
+
+    addCartItem(item) {
+        const div = document.createElement("div");
+
+        div.classList.add('cart-item');
+        div.innerHTML = `
+                    <img src=${item.image} alt="product">
+                    <div>
+                        <h4>${item.title}</h4>
+                        <h5>$${item.price}</h5>
+                        <span class="remove-item" data-id=${item.id}>remove</span>
+                    </div>
+                    <div>
+                        <i class="fas fa-chevron-up" data-id=${item.id}></i>
+                        <p>${item.amount}</p>
+                        <i class="fas fa-chevron-down" data-id=${item.id}></i>
+                    </div>`;
+                    cartContent.appendChild(div);
+                    console.log(cartContent);
+
     }
 
 }
