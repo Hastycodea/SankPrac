fe35e946498b43c3ea19368062bc5f830db5e5a2
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-09-22 16:27:56 +0300

    ajax

--- JQuery/index.js
+++ JQuery/index.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     $("#about").click(function() {
-        $(this).html('you clicked me!');
+        
+        if($(this).html() === "you clicked me!") {
+            $(this).html("you clicked me again!");
+        } else {
+            $(this).html('you clicked me!');
+        }
+        
     });
 
     $("#about").hover(function() {
@@ -25,5 +31,34 @@ $(document).ready(function() {
 
     $(".power").keyup(function (){
         $("#about").html($(this).val());
+    });
+
+    $("#getjson").click(function() {
+        $.ajax({url: 'games.json', success: function(result) {
+            $("#about").html(JSON.stringify(result, null, 4));
+        }});
+    });
+
+    $("#getgame").click(function() {
+        $.ajax({url: 'games.json', success: function(result) {
+            var game = result.games[0].genre;
+            $("#about").html(game);
+        }});
+    });
+
+    $("#allgames").click(function() {
+        $.ajax({
+            url: 'games.json',
+            success: function(result) {
+                var games = result.games;
+                var gamesList = '';
+
+                games.forEach(function(game) {
+                    gamesList += game.title + "<br>";
+                });
+
+                $("#about").html(gamesList);
+            }
+        });
     });
 }); 
