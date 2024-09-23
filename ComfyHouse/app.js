e6e54f4c00bdb03b4bbb721ccbe9ddcb62be0623
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-09-23 09:48:39 +0300

    clearcart

--- ComfyHouse/app.js
+++ ComfyHouse/app.js
@@ -49,7 +49,7 @@ class UI {
                     <img src=${product.image} alt="product" class="product-img">
                     <button class="bag-btn" data-id=${product.id}>
                         <i class="fas fa-shopping-cart"></i>
-                        add to bag
+                        add to cart
                     </button>
                 </div>
                 <h3>${product.title}</h3>
@@ -159,6 +159,41 @@ class UI {
         cart.forEach(item => this.addCartItem(item));
     }
 
+    cartLogic() {
+        // clear cart button
+        clearCartBtn.addEventListener('click', () => {
+            this.clearCart();
+        });
+
+        // cart functionality
+
+    }
+
+    clearCart() {
+        let cartItems = cart.map(item => item.id);
+        cartItems.forEach(id => this.removeItem(id));
+        console.log(cartContent.children);
+
+        while(cartContent.children.length > 0) {
+            cartContent.removeChild(cartContent.children[0]);
+        }
+
+        this.hideCart();
+    }
+
+    removeItem(id) {
+        cart = cart.filter(item => item.id !== id);
+        this.setCartValues(cart);
+        Storage.saveCart(cart);
+        let button = this.getSingleButton(id);
+        button.innerHTML = `<i class="fas fa-shopping-cart"></i>add to cart`
+        button.disabled = false;
+    }
+
+    getSingleButton(id) {
+        return buttonsDOM.find(button => button.dataset.id === id);
+    }
+
 }
 
 //local storage
@@ -193,7 +228,10 @@ document.addEventListener("DOMContentLoaded", () => {
     products.getProducts().then(products => {
         ui.displayProducts(products);
         Storage.saveProducts(products);  
-    }).then(() => ui.getBagButtons());
+    }).then(() => {
+        ui.getBagButtons();
+        ui.cartLogic();
+    });
 
 });
 
