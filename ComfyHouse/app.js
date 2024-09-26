398b29416005cc9ebc92c0fed798b3c6dcc043eb
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-09-26 14:57:16 +0300

    cart updated

--- ComfyHouse/app.js
+++ ComfyHouse/app.js
@@ -51,6 +51,7 @@ class UI {
                         <i class="fas fa-shopping-cart"></i>
                         add to cart
                     </button>
+                    
                 </div>
                 <h3>${product.title}</h3>
                 <h4>Ksh ${product.price}</h4>
@@ -172,7 +173,7 @@ class UI {
     clearCart() {
         let cartItems = cart.map(item => item.id);
         cartItems.forEach(id => this.removeItem(id));
-        console.log(cartContent.children);
+        // console.log(cartContent.children);
 
         while(cartContent.children.length > 0) {
             cartContent.removeChild(cartContent.children[0]);
