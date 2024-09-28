14d24bf62abbd7e555e427e46d25f490d7760c63
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-09-28 20:58:55 +0300

    adding to cart

--- JqueryPrac/index.js
+++ JqueryPrac/index.js
@@ -6,13 +6,14 @@ $(document).ready(function() {
             var productsList = '';
 
             products.forEach(function(product){
+                console.log(product.id)
                 productsList += '<div class="col-12 col-lg-2">' +
                 '<div class="card" style="width: 18rem;">' +
                     '<img src="'+ product.image +'" class="card-img-top" alt="...">' +
                     '<div class="card-body">' +
                         '<h5 class="card-title">'+ product.title +'</h5>' +
-                        '<p class="card-text">Ksh.'+ product.price +'</p>' +
-                        '<a href="#" class="btn btn-primary">Add to Cart</a>' +
+                        '<p class="card-text">Ksh. '+ product.price +'</p>' +
+                        '<button class="btn btn-primary cart-btn" data-id='+ product.id +'>Add to Cart</button>' +
                     '</div>' +
                 '</div>' +
             '</div>';
@@ -21,5 +22,37 @@ $(document).ready(function() {
             $('#productItem').html(productsList);
 
         }
+    });
+
+    $('#productItem').on('click', '.cart-btn',function() {
+        var productId = $(this).data('id');
+        console.log(productId);
+
+        $.ajax({
+            url : 'products.json',
+            success : function(result) {
+                var products = result.items;
+                console.log(products);
+                var product = products.find(product => product.id === productId);
+
+                $('#cart-item').append(`
+                    <div class="col-12 col-lg-4 d-flex align-items-center gap-2">
+                        <img src="" alt="..." class="cart-image">
+                        <p id="productName">${product.title}</p>
+                    </div>
+                
+                    <div class="col-12 col-lg-4 d-flex gap-2 align-items-center">
+                        <button class="btn btn-outline-secondary"> - </button>
+                        <span>1</span>
+                        <button class="btn btn-outline-secondary"> + </button>
+                    </div>
+                
+                    <div class="col-12 col-lg-4 d-flex align-items-center">
+                        <p>Ksh. <span id="productPrice">${product.price}</span></p>
+                    </div>
+                    <hr class="mt-2"></hr>
+                `);
+            }
+        });
     });
 });
