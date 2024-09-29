fefd8a934d3b580606d640cb2246a7aadbf23b98
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-09-29 11:35:09 +0300

    ReactDive

---
+++ NodeReact/my-app/src/App.js
@@ -0,0 +1,15 @@
+import MyHome from './pages/Home';
+import Nav from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <MyHome />
+      <h1>My first react app</h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ NodeReact/my-app/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../css/Homestyle.css'
+
+const Home = () => {
+  return (
+    <div className='container'>
+        <div className='text'>My very educated mother just bla blas kkd lorem40 dididididi jjjdikk kkkkkkkkkkkkkkk kllllllllllllllllllk kkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkklll </div>
+
+        <div className='text'>My very educated mother just bla blas kkd lorem40 dididididi jjjdikk kkkkkkkkkkkkkkk kllllllllllllllllllk kkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkklll </div>
+
+        <div className='text'>My very educated mother just bla blas kkd lorem40 dididididi jjjdikk kkkkkkkkkkkkkkk kllllllllllllllllllk kkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkklll </div>
+
+        <div className='text'>My very educated mother just bla blas kkd lorem40 dididididi jjjdikk kkkkkkkkkkkkkkk kllllllllllllllllllk kkkkkkkkkkkkkkkkk kkkkkkkkkkkkkkkkkkkkklll </div>
+        
+    </div>
+  )
+}
+
+export default Home
