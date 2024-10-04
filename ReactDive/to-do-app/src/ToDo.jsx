b00747342868f579f3c13c2f4ff6962b6cb51179
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-10-04 12:20:46 +0300

    updated add

--- ReactDive/to-do-app/src/ToDo.jsx
+++ ReactDive/to-do-app/src/ToDo.jsx
@@ -2,23 +2,42 @@ import React, { useState } from 'react';
 
 function ToDo() {
 
+    const [tasks, setTasks] = useState([]);
+
+    function handleAddTask() {
+        const newTask = document.getElementById('new-task').value;
+
+        setTasks(t => [...t, newTask]);
+
+        document.getElementById('new-task').value = '';
+    }
+
+    function handleDeleteTask(id) {
+        setTasks(tasks.filter((_, i) => i !== id ));
+    }
+
     return (
         <div className='todo'>
             <h2 className='title'>To-Do-List</h2>
             <div className="task-input">
-                <input type="text" placeholder='Enter a task' />
-                <button>Add</button>
+                <input type="text" placeholder='Enter a task' id='new-task' />
+                <button onClick={handleAddTask}>Add</button>
             </div>
 
-            <div className="task-items">
-                <p>Walk The Dog</p>
+            {tasks.map((task, index) => <div className="task-items">
+                <p>{task}</p>
 
                 <div>
                     <button className='delete'>Delete</button>
                     <button className='position'>up</button>
                     <button className='position'>down</button>
                 </div>
-            </div>
+
+                
+            </div>)}
+            
+
+            
         </div>
     );
 }
