4ecd85b3783075148cdd1bd8239b91a7abf07c24
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-10-12 12:04:27 +0300

    pythonUpdated

--- PythonDive/main.py
+++ PythonDive/main.py
@@ -1,3 +1,53 @@
 
 print("Hello Andreh!")
 
+
+# Identifiers/variables = store the values
+# Rules for Defining Identifiers
+# => Alphabet symbols(Uppercase or Lowercase)
+# => Starts with _(underscore)
+# => Starts with numbers not allowed
+# => Identifiers are case sensitive
+# => Keywords are not used
+# => Speicial characters are not allowed inside anywhere in variable name
+
+## Examples
+# Amor, amor, _amor, first_last, firstName
+
+
+## Data Types
+a = 10 
+b = "10"
+c = 10.01
+d = 2 + 3j
+e = False
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+print(d.real)
+print(d.imag)
+
+# Numerical Data Type:
+#Integer
+#Float
+#range
+#byte
+
+
+#Operators
+# Arithmetic Operators => + , - , / , */ %
+# Relational Operators => >, <, <=, >=, !=
+# Logical Operators => AND, OR, NOT, XOR, NAND
+# Bitwise Operators => &, |, ~(Complimenst), <<, >> (shift operators)
+# Assignement Operators => =
+# Ternary Operator => (?:) leftside : rightside condition
+
+
+# TYPE CASTING => converting one data type into another data type
+num = '23'
+num = int(num)
+print(type(num))
